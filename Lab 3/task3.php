54aba293e826155a3fe41c09d73bd08932857d98
tawhid-k <tawhid.khondakar18@gmail.com>
2020-10-27 16:46:36 +0600

    Lab 3 task

---
+++ Lab 3/task3.php
@@ -0,0 +1,94 @@
+<html>
+	<title>
+		Register page
+	</title>
+	<hr>
+	<form action="" method="post">
+		<fieldset>
+			<legend>
+				<h1>
+					Club Member Registration
+				</h1>
+			</legend>
+				<table>
+					<tr>
+						<td align="right">
+							Name:
+						</td>
+						<td>
+							<input type="text" name="name">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Username:
+						</td>
+						<td>
+							<input type="text" name="username">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Password:
+						</td>
+						<td>
+							<input type="Password" name="pass">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+						     Confirm Password:
+						</td>
+						<td>
+							<input type="Password" name="upass">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Email:
+						</td>
+						<td>
+							<input type="text" name="email">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Phone:
+						</td>
+						<td>
+							<input type="text" size="5" placeholder="Code"> - <input type="text" size="7" placeholder="Number">
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Address:
+						</td>
+						<td>
+							<input type="text" name="address" placeholder="Street Address">
+						</td>
+					</tr>
+					<tr><td></td>
+						<td>	
+						   <input type="text" name="city" size="7" placeholder="City"> - <input type="text" name="state" size="5" place>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>
+							<input type="text" name="zipCode" placeholder="Postal/Zip Code">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Birth Date:
+						</td>
+						<td>
+							<select>
+								<option></option>
+							</select>
+						</td>
+					</tr>
+				</table>
+		</fieldset>
+	</form>
+</html>
