27d087600ffdf33081a7c3b400f58cb0f940bcdc
tawhid-k <tawhid.khondakar18@gmail.com>
2020-10-27 18:49:03 +0600

    Lab 3 task

--- Lab 3/task1.php
+++ Lab 3/task1.php
@@ -79,13 +79,68 @@
 						</td>
 					</tr>
 					<tr>
-						<td>
+						<td align="right">
 							Birth Date:
 						</td>
 						<td>
 							<select>
-								<option></option>
+								<option disabled="disabled" selected="selected"> Day </option>
+								<?php
+                                   for($i = 1; $i <= 31; $i++) {
+                                   	 echo "<option>" . "$i" . "</option>";
+                                   }
+								?>
 							</select>
+							<select>
+								<option disabled="disabled" selected="selected"> Month </option>
+								<?php
+								$month = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "Octobor", "November", "December");
+                                   for($i = 0; $i < 12; $i++) {
+                                   	 echo "<option> $month[$i] </option>";
+                                   }
+								?>
+							</select>
+							<select>
+								<option disabled="disabled" selected="selected"> Year </option>
+								<?php
+								  for ($i = 0; $i < 30; $i++) {
+								  	echo "<option>" . (1990+$i) . "</option>";
+								  }
+ 								?>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Gender:
+						</td>
+						<td>
+							<input type="radio" name="gender"> Male
+							<input type="radio" name="gender"> Female
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Where did you hear about us?
+						</td>
+						<td>
+							<input type="checkbox" name="friend"> A Friend of Colleague<br>
+							<input type="checkbox" name="friend"> Google <br>
+							<input type="checkbox" name="friend"> Blog Post <br>
+							<input type="checkbox" name="friend"> News Article <br>
+						</td>
+					</tr>
+					<tr>
+						<td align="right">
+							Bio:
+						</td>
+						<td>
+							<textarea cols="20" rows="3"></textarea>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2" align="center">
+							<input type="submit" name="submit" value="register">
 						</td>
 					</tr>
 				</table>
