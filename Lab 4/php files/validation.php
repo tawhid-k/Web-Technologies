c570d4ea3b18e3359750139162011eb87178147d
tawhid-k <tawhid.khondakar18@gmail.com>
2020-11-03 20:52:29 +0600

    Lab Task

--- Lab 4/php files/validation.php
+++ Lab 4/php files/validation.php
@@ -16,7 +16,6 @@
        $file = $file->user;
        $is_success = false;
        foreach ($file as $i) {
-       	var_dump($i->name);
        	 if (($i->name == $_POST["uname"]) && ($i->pass == $_POST["pass"])) {
 
        	   $is_success = true;
