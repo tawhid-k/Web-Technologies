d48df639d5d496b6daca01cbffa3c46e2705dfb0
tawhid-k <tawhid.khondakar18@gmail.com>
2020-12-10 22:44:15 +0600

    Whole completed

--- Final term/Lab 10.12.20/controller/userController.php
+++ Final term/Lab 10.12.20/controller/userController.php
@@ -6,8 +6,20 @@
   	 }
   	 else echo "Wrong username or password";
   }
-  if (isset($_POS["register"])) {
-    echo "Hello World";
+  if (isset($_POST["change"])) {
+     $id = $_GET["id"];
+     updateStudent($_POST["name"], $id, $_POST["dob"], $_POST["credit"], $_POST["cgpa"], $_POST["did"]);
+     header("Location: show_students.php");
+  }
+  if (isset($_POST["register"])) {
+     addStudent($_POST["name"], $_POST["id"], $_POST["dob"], $_POST["credit"], $_POST["cgpa"], $_POST["did"]);
+     header("Location: dashboard.php");
+  }
+  function getStudentData() {
+     return getResult("select * from student");
+  }
+  function getDepartmentData() {
+     return getResult("select * from department");
   }
   function isUser ($u, $p) {
       $p = md5($p);
@@ -15,5 +27,14 @@
       $data = getResult($q);
       if (count($data) > 0) return true;
       else return false;
-   }
+  }
+  function addStudent($n, $id, $dob, $c, $cg, $did) {
+      runQuery("insert into student values('".$n."','".$id."','".$dob."',".$c.",".$cg.",".$did.")");
+  }
+  function updateStudent($n, $id, $dob, $c, $cg, $did) {
+      runQuery("update student set name='$n',dob='$dob',credit=$c,cgpa=$cg,dept_id=$did where id='$id'");
+  }
+  function deleteStudent($id) {
+      runQuery("delete from student where id = '$id'");
+  }
 ?>
--- Final term/Lab 10.12.20/dashboard.php
+++ Final term/Lab 10.12.20/dashboard.php
@@ -9,7 +9,7 @@
     	<li>1. <a href="show_students.php">Show all students</a> </li>
     	<li>2. <a href="show_departments.php">Show all departments</a> </li>
     	<li>3. <a href="add_student.php">Add new student</a> </li>
-    	<li>4. <a href="edit_student.php">Edit student</a> </li>
+    	<!--<li>4. <a href="edit_student.php">Edit student</a> </li>!-->
     </ui>
 </body>
 </html>
